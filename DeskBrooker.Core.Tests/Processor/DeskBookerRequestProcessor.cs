83c1e1b4137dc2e8a8f56914db52d99d20616dcd
Lawrence LCI <lcheunglci@gmail.com>
2022-04-08 02:31:24 -0700

    Add project files.

---
+++ DeskBrooker.Core.Tests/Processor/DeskBookerRequestProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DeskBooker.Core.Processor
+{
+    internal class DeskBookerRequestProcessor
+    {
+        public DeskBookerRequestProcessor()
+        {
+        }
+
+        internal DeskBookingResult BookDesk(DeskBookingRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DeskBrooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            // Arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "testing@test.com",
+                Date = new DateTime(2022, 1, 1)
+            };
+
+            var processor = new DeskBookerRequestProcessor();
+
+            // Act
+            DeskBookingResult result = processor.BookDesk(request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+
+
+        }
+    }
+}
