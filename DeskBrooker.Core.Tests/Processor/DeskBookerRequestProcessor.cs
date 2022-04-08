42cd21521412d4bc7c2a48eacacb9dec234ec645
Lawrence LCI <lcheunglci@gmail.com>
2022-04-08 08:39:30 -0700

    Add impl to BookDesk to pass the first test

--- DeskBrooker.Core.Tests/Processor/DeskBookerRequestProcessor.cs
+++ DeskBrooker.Core.Tests/Processor/DeskBookerRequestProcessor.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace DeskBooker.Core.Processor
+﻿namespace DeskBooker.Core.Processor
 {
     internal class DeskBookerRequestProcessor
     {
@@ -10,7 +8,13 @@ namespace DeskBooker.Core.Processor
 
         internal DeskBookingResult BookDesk(DeskBookingRequest request)
         {
-            throw new NotImplementedException();
+            return new DeskBookingResult
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Date = request.Date,
+            };
         }
     }
 }
