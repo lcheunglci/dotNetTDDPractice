bb493ca21d4582e58f134aa107d559ca3d7ff44e
Lawrence LCI <lcheunglci@gmail.com>
2022-04-09 23:36:33 -0700

    Refactor the test to move the processor initialization to the constructor

--- DeskBrooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
+++ DeskBrooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -6,6 +6,13 @@ namespace DeskBooker.Core.Processor
 {
     public class DeskBookingRequestProcessorTests
     {
+        private readonly DeskBookerRequestProcessor _processor;
+
+        public DeskBookingRequestProcessorTests()
+        {
+            _processor = new DeskBookerRequestProcessor();
+        }
+
         [Fact]
         public void ShouldReturnDeskBookingResultWithRequestValues()
         {
@@ -18,10 +25,8 @@ namespace DeskBooker.Core.Processor
                 Date = new DateTime(2022, 1, 1)
             };
 
-            var processor = new DeskBookerRequestProcessor();
-
             // Act
-            DeskBookingResult result = processor.BookDesk(request);
+            DeskBookingResult result = _processor.BookDesk(request);
 
             // Assert
             Assert.NotNull(result);
@@ -34,8 +39,7 @@ namespace DeskBooker.Core.Processor
         [Fact]
         public void ShouldThrowExceptionIfRequestIsNull()
         {
-            var processor = new DeskBookerRequestProcessor();
-            var exception = Assert.Throws<ArgumentNullException>(() => processor.BookDesk(null));
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.BookDesk(null));
 
             Assert.Equal("request", exception.ParamName);
         }
