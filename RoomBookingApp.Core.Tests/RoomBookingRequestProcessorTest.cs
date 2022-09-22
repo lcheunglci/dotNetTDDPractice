259114ddad853b1af192e20808b1d9768aef5e92
Lawrence LCI <lcheunglci@gmail.com>
2022-09-21 23:42:08 -0700

    Add xunit project for a room booking system

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,15 @@
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void Should_Return_Room_Booking_Request()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
