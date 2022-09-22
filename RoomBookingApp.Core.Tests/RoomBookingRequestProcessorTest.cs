890da5ff1b2cf9d6c221a0befab48467a3f97891
Lawrence LCI <lcheunglci@gmail.com>
2022-09-21 23:52:51 -0700

    Create a failed test for booking room

--- RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -1,15 +1,37 @@
+using Shouldly;
+
 namespace RoomBookingApp.Core
 {
     public class RoomBookingRequestProcessorTest
     {
         [Fact]
-        public void Should_Return_Room_Booking_Request()
+        public void Should_Return_Room_Booking_Request_With_Values()
         {
             // Arrange
+            var request = new RoomBookingRequest
+            {
+                FullName = "Test Name",
+                Email = "test@request.com",
+                Date = new DateTime(2022, 9, 20)
+            };
+
+
+            var processor = new RoomBookingRequestProcessor();
 
             // Act
+            RoomBookingResult result = processor.BookRoom(request);
 
             // Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FullName, request.FullName);
+            Assert.Equal(request.Email, request.Email);
+            Assert.Equal(request.Date, request.Date);
+
+            result.ShouldNotBeNull();
+            result.FullName.ShouldBe(request.FullName);
+            result.Email.ShouldBe(request.Email);
+            result.Date.ShouldBe(request.Date);
+
         }
     }
 }
