4a4eb65c8ee4842e1a1fdffc52a25084250abd27
Lawrence LCI <lcheunglci@gmail.com>
2023-08-12 00:06:27 -0700

    Add test to check remaining cups in stock and add logic to make test pass

--- WireBrainCoffee.CupOrderAdmin.Core.Tests/OrderCreationServiceTests.cs
+++ WireBrainCoffee.CupOrderAdmin.Core.Tests/OrderCreationServiceTests.cs
@@ -8,30 +8,59 @@ namespace WireBrainCoffee.CupOrderAdmin.Core.Tests
     [TestClass]
     public class OrderCreationServiceTests
     {
-        [TestMethod]
-        public async Task ShouldStoreCreatedOrderInOrderCreationResult()
+        private OrderCreationService _orderCreationService;
+        private int _numberOfCupsInStock;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            _numberOfCupsInStock = 10;
             var orderRepositoryMock = new Mock<IOrderRepository>();
             orderRepositoryMock.Setup(x =>
             x.SaveAsync(It.IsAny<Order>())).ReturnsAsync((Order x) => x);
 
-
-
             var coffeeCupRepositoryMock = new Mock<ICoffeeCupRepository>();
-            
-            var orderCreationService = new OrderCreationService(orderRepositoryMock.Object, coffeeCupRepositoryMock.Object);
+            coffeeCupRepositoryMock.Setup(x => x.GetCoffeeCupsInStockCountAsync())
+                .ReturnsAsync(_numberOfCupsInStock);
+
+            _orderCreationService = new OrderCreationService(orderRepositoryMock.Object, coffeeCupRepositoryMock.Object);
+
+        }
+
+        [TestMethod]
+        public async Task ShouldStoreCreatedOrderInOrderCreationResult()
+        {
 
             var numberOfOrderedCups = 1;
-            var customer = new Customer { Id = 99 } ;
-
+            var customer = new Customer { Id = 99 };
 
             var orderCreationResult =
-                await orderCreationService.CreateOrderAsync(customer, numberOfOrderedCups);
+                await _orderCreationService.CreateOrderAsync(customer, numberOfOrderedCups);
 
             Assert.AreEqual(OrderCreationResultCode.Success, orderCreationResult.ResultCode);
             Assert.IsNotNull(orderCreationResult.CreatedOrder);
             Assert.AreEqual(customer.Id, orderCreationResult.CreatedOrder.CustomerId);
 
         }
+
+        [TestMethod]
+        public async Task ShouldStoreRemainingCupsInStockInOrderCreationResult()
+        {
+            var numberOfOrderedCups = 3;
+            var expectedRemainingCupsInStock = _numberOfCupsInStock - numberOfOrderedCups;
+            var customer = new Customer();
+
+            var orderCreationResult =
+                await _orderCreationService.CreateOrderAsync(customer, numberOfOrderedCups);
+
+            Assert.AreEqual(OrderCreationResultCode.Success, orderCreationResult.ResultCode);
+            Assert.AreEqual(expectedRemainingCupsInStock, orderCreationResult.RemainingCupsInStock);
+
+
+
+
+        }
     }
+
+
 }
