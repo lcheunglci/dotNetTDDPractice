5e8363f8ba2fa2ae40ab1a0336babedb06593386
Lawrence LCI <lcheunglci@gmail.com>
2023-08-11 23:30:42 -0700

    Add first test to the order creation

--- WireBrainCoffee.CupOrderAdmin.Core.Tests/OrderCreationServiceTests.cs
+++ WireBrainCoffee.CupOrderAdmin.Core.Tests/OrderCreationServiceTests.cs
@@ -1,12 +1,27 @@
+using WiredBrainCoffee.CupOrderAdmin.Core.Model;
+using WiredBrainCoffee.CupOrderAdmin.Core.Services.OrderCreation;
+
 namespace WireBrainCoffee.CupOrderAdmin.Core.Tests
 {
     [TestClass]
     public class OrderCreationServiceTests
     {
         [TestMethod]
-        public void ShouldStoreCreatedOrderInOrderCreationResult()
+        public async Task ShouldStoreCreatedOrderInOrderCreationResult()
         {
-            throw new NotImplementedException();
+            var orderCreationService = new OrderCreationService(null, null);
+
+            var numberOfOrderedCups = 1;
+            var customer = new Customer();
+
+
+            var orderCreationResult =
+                await orderCreationService.CreateOrderAsync(customer, numberOfOrderedCups);
+
+            Assert.AreEqual(OrderCreationResultCode.Success, orderCreationResult.ResultCode);
+            Assert.IsNotNull(orderCreationResult.CreatedOrder);
+            Assert.AreEqual(customer.Id, orderCreationResult.CreatedOrder.CustomerId);
+
         }
     }
 }
