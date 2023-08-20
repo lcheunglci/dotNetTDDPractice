2967f8aa4e2dcbb888cd2cb847cd39e943e06559
Lawrence LCI <lcheunglci@gmail.com>
2023-08-20 00:10:39 -0700

    Add new project for unit testing

---
+++ WiredBrainCoffee.DataProcessor/Model/MachineDataItem.cs
@@ -0,0 +1,15 @@
+﻿namespace WiredBrainCoffee.DataProcessor.Model
+{
+    public class MachineDataItem
+    {
+        public MachineDataItem(string coffeeType, DateTime createdAt)
+        {
+            CoffeeType = coffeeType;
+            CreatedAt = createdAt;
+        }
+
+        public string CoffeeType { get; }
+
+        public DateTime CreatedAt { get; }
+    }
+}
---
+++ WiredBrainCoffee.DataProcessor/Parsing/CsvLineParser.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using WiredBrainCoffee.DataProcessor.Model;
+
+namespace WiredBrainCoffee.DataProcessor.Parsing
+{
+    public class CsvLineParser
+    {
+        public static MachineDataItem[] Parse(string[] csvlines)
+        {
+            var machineDataItems = new List<MachineDataItem>();
+
+            foreach (var csvLine in csvlines)
+            {
+                var machineDataItem = Parse(csvLine);
+
+                machineDataItems.Add(machineDataItem);
+            }
+
+            return machineDataItems.ToArray();
+        }
+
+        private static MachineDataItem Parse(string csvLine)
+        {
+            var lineItems = csvLine.Split(';');
+
+            return new MachineDataItem(lineItems[0], DateTime.Parse(lineItems[1], CultureInfo.InvariantCulture));
+        }
+    }
+}
