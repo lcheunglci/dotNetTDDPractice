2ced4b701957232f9f1151eb5213b0b01bc6c273
Lawrence LCI <lcheunglci@gmail.com>
2023-08-23 22:45:15 -0700

    Fix syntax error

--- WiredBrainCoffee.DataProcessorTests/CsvLineParserTests.cs
+++ WiredBrainCoffee.DataProcessorTests/CsvLineParserTests.cs
@@ -6,7 +6,7 @@ public class CsvLineParserTest
     public void ShouldParseValidLine()
     {
         // arrange
-        string[] csvLines = new [] {"Cappuccino;10/27/2022 8:06:04 AM"};
+        string[] csvLines = new[] { "Cappuccino;10/27/2022 8:06:04 AM" };
 
         // act
         var machineDataItems = CsvLineParser.Parse(csvLines);
@@ -15,14 +15,14 @@ public class CsvLineParserTest
         Assert.NotNull(machineDataItems);
         Assert.Single(machineDataItems);
         Assert.Equal("Cappuccino", machineDataItems[0].CoffeeType);
-        Assert.Equal(new DateTime(2022,10,27,8,6,4), machineDataItems[0].CreatedAt);
+        Assert.Equal(new DateTime(2022, 10, 27, 8, 6, 4), machineDataItems[0].CreatedAt);
     }
 
     [Fact]
-    public void ShouldSkipEmptyLines{}
+    public void ShouldSkipEmptyLines()
     {
         // arrange
-        string[] csvLines = new [] { "" };
+        string[] csvLines = new[] { "" };
 
         // act
         var machineDataItems = CsvLineParser.Parse(csvLines);
@@ -30,6 +30,7 @@ public class CsvLineParserTest
         // assert
         Assert.NotNull(machineDataItems);
         Assert.Empty(machineDataItems);
-        
+
     }
+
 }
